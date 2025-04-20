a30b38a72a829ea6075c89793c74c75cdff7942b
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2025-04-20 17:08:03 -0400

    BRR: basic front end: Home.js and styling. backend: userAuth

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 const connectDb = require("./config/connectionDb");
+const cors=require("cors")
 
 const PORT = process.env.PORT || 3000;
 connectDb();
-app.use(express.json());
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
 
 app.use("/recipe", require("./routes/resipe"));
 
